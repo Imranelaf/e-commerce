017922712aec57fb5b5e2bc2aab3f1c584923c81
imrane ALI LAFKIH <imrane.laf@gmail.com>
2025-03-22 23:52:49 +0000

    fix: Make the footer responsive on mobile devices

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,38 +1,41 @@
-export default function Footer(){
-
-    const help = ["1-000-000-000",
+export default function Footer() {
+    const help = [
+        "1-000-000-000",
         "1-111-000-1111 (Text)",
         "help@mysite.com",
         "Returns/Exchanges",
-        "FAQ/Contact Us"];
-     const shop = [
+        "FAQ/Contact Us"
+    ];
+    const shop = [
         "Men's Shoes",
         "Women's Shoes",
         "Socks"
-        ]   
+    ];
 
-    return(
-         <div className="w-full h-auto bg-gray-500 flex justify-around">
-            <div>
-                <h1>HELP</h1>
-                {
-                    help.map((item, index)=>(
-                        <p key={index}>{item}</p>
-                    ))
-                }
+    return (
+        <div>
+            <div className="w-full h-auto bg-gray-800 flex justify-around p-4">
+                <div>
+                    <h1 className="m-2 font-bold text-white">HELP</h1>
+                    {help.map((item, index) => (
+                        <p className="cursor-pointer m-2 italic text-white" key={index}>
+                            {item}
+                        </p>
+                    ))}
+                </div>
+                <div>
+                    <h1 className="m-2 font-bold text-white">SHOP</h1>
+                    {shop.map((item, index) => (
+                        <p className="cursor-pointer m-2 italic text-white" key={index}>
+                            {item}
+                        </p>
+                    ))}
+                </div>
             </div>
-            <div>
-                <h1>SHOP</h1>
-                {
-                    shop.map((item, index)=>(
-                        <p key={index}>{item}</p>
-                    ))
-                }
+
+            <div className="text-center py-2 bg-gray-800 text-white">
+                <p>&copy; {new Date().getFullYear()} All rights reserved</p>
             </div>
-
-         </div>   
-
-    )
-
-
+        </div>
+    );
 }
