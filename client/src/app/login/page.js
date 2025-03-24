450e6cb73d9c690903b73b03ff34815fde82cb72
imrane ALI LAFKIH <imrane.laf@gmail.com>
2025-03-24 23:57:25 +0000

    feat: Creat the login page

---
+++ client/src/app/login/page.js
@@ -0,0 +1,47 @@
+'use client'
+
+import { useState } from "react";
+
+export default function Login() {
+    const [inputValue, setInputValue] = useState('');
+    const [isButtonActive, setIsButtonActive] = useState(false);
+
+    const checkEmail = (event) => {
+        const email = event.target.value;
+        setInputValue(email);
+        setIsButtonActive(email.includes('@')); 
+    };
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-gray-100">
+            <div className="w-2/5 h-auto m-3 p-4 bg-white">
+                <div className="flex w-full justify-center">
+                    <img
+                        src="/images/logo.png"
+                        width={200}
+                        height={200}
+                        loading="lazy"
+                        alt="Logo"
+                    />
+                </div>
+                <h1 className="text-2xl font-bold m-5">Log in</h1>
+                <div className="flex flex-col">
+                    <input
+                        type="email"
+                        className="border m-5 p-2 border-gray-400 text-gray-900 hover:border-black"
+                        placeholder="Email"
+                        value={inputValue}
+                        onChange={checkEmail}
+                    />
+                    <button
+                        className={isButtonActive ? 'bg-gray-900 cursor-pointer m-5 text-xl text-white font-bold p-2' :
+                            'bg-gray-200 m-5 text-xl text-gray-500 font-bold p-2 cursor-not-allowed'}
+                        disabled={!isButtonActive}
+                    >
+                        Continue
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
